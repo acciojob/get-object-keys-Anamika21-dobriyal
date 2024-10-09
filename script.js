f00aaddd36b0f43927234161b7417c92f013cef0
Anamika <anamikadobriyal21@gmail.com>
2024-10-09 23:51:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+	name: "Alice"
+};
+
+Object.prototype.getKeys = function () {
+	return Object.keys(this);
+};
+
+let keys = student.getKeys();
+console.log(keys);
